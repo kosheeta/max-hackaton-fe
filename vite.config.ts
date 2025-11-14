5abd6c387390015741ac984ace70efa5a0dac85b
Nikita Koshelenko <nikitakoschelenko@gmail.com>
2025-11-14 19:39:57 +0300

    feat: add image optimizer plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    ViteImageOptimizer({
+      jpeg: { quality: 80 },
+      png: { compressionLevel: 8 },
+      webp: { quality: 80 },
+    }),
+  ],
 })
